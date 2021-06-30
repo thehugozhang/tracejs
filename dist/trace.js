4a0fa6064e2aafcdeae4362190cb0a76a7c07649
Hugo Zhang <hockeysk8r.hugo@gmail.com>
2021-06-29 21:55:57 -0400

    Update trace.js

--- dist/trace.js
+++ dist/trace.js
@@ -1,1 +1,56 @@
+class tracejs {
+	static init(config) {
+		console.log("Here is the user id: " + config.id) //validate user id before allowing functionality
+		console.log("Here is the project id: " + config.project_id)
+		this.id = config.id;
+		this.pid = config.project_id;
+	}
 
+	static report(code, tag) {
+		var validation = new URL('https://tracejs.co/api/v1/validate')
+		var whitelist = {url: this.getURL()}
+
+		// Passing url queries
+		validation.search = new URLSearchParams(whitelist).toString();
+
+		// API call using Fetch API (No IE11 support)
+		fetch(validation).then(response => response.json())
+		.then(data => {
+			console.log('Success:', data);
+			if (data.result) { // add || statement later for open security
+				// Authorized API call
+				var call = new URL('https://tracejs.co/api/v1/report')
+				var param = {code: code, tag: tag} 
+
+				//passing url queries
+				call.search = new URLSearchParams(param).toString();
+
+				//api call using Fetch API (No IE11 support)
+				fetch(call).then(response => response.json())
+				.then(data => {
+					console.log('Success:', data);
+				})
+				.catch((error) => {
+					console.error('Error:', error);
+				});
+			} else {
+				// Validation determined unauthorized API call
+				console.error(data.log)
+			}
+		})
+		.catch((error) => {
+			console.error('Error:', error);
+		});
+	}
+
+	static getURL() {
+		return window.location.href;
+	}	
+}
+
+window.onerror = function (msg, url, lineNo, columnNo, error) {
+    var tag = url + " @ Ln " + lineNo + " Col " + columnNo
+    var code = msg
+    tracejs.report(code, tag);
+    return false;
+}
