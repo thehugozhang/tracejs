f012d01c40c0340160ddbfa224b25a126f7b4b00
Hugo Zhang <hockeysk8r.hugo@gmail.com>
2021-07-07 00:06:23 -0400

    Update trace.js

--- dist/trace.js
+++ dist/trace.js
@@ -48,34 +48,14 @@ class tracejs {
 	}	
 }
 
-// Returns browser info in format:
-// {
-//   "name": "Chrome",
-//   "version": "91"
-// }
-function getBrowser() {
-    var ua=navigator.userAgent,tem,M=ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || []; 
-    if(/trident/i.test(M[1])){
-        tem=/\brv[ :]+(\d+)/g.exec(ua) || []; 
-        return {name:'IE',version:(tem[1]||'')};
-        }   
-    if(M[1]==='Chrome'){
-        tem=ua.match(/\bOPR|Edge\/(\d+)/)
-        if(tem!=null)   {return {name:'Opera', version:tem[1]};}
-        }   
-    M=M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
-    if((tem=ua.match(/version\/(\d+)/i))!=null) {M.splice(1,1,tem[1]);}
-    return {
-      name: M[0],
-      version: M[1]
-    };
- }
-
 window.onerror = function (msg, url, lineNo, columnNo, error) {
-    var browser = getBrowser()
     var tag = url + " @ Ln " + lineNo + " Col " + columnNo
-    var code = browser.name + " " + browser.version + " " + msg
-    console.log(code)
+    var code = msg + ' ' + error
+    console.log(msg)
+    console.log(url)
+    console.log(lineNo)
+    console.log(columnNo)
+    console.log(error)
     tracejs.report(code, tag);
     return false;
 }
